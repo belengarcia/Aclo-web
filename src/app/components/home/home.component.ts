4ff1cc0983c68fb3467e8bc2b1c900cd86858ec6
Belen <>
2018-10-10 23:51:17 +0200

    componente home con dudas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,8 @@
+import { FuckOff } from './../../shared/models/fuckOffs.model';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { FuckOffsService } from './../../shared/services/fuck-offs.service';
+import { ApiError } from './../../shared/models/ApiErro.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  fuckOff: FuckOff = new FuckOff();
+  apiError: ApiError;
+  userId: String; //de donde lo saco?
+
+  constructor(private fuckOffsService: FuckOffsService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onClickShitButton(form: FormGroup): void {
+    if(form.valid){
+      this.fuckOffsService.fy(this.userId, this.fuckOff)
+        .subscribe(
+          () => {
+            form.reset()
+            this.router.navigate(['/destinies'])
+          }
+        )
+    }
   }
 
 }
