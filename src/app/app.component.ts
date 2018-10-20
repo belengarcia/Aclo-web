0a43f4f09055f5904fa6cf3217bbe651d312332e
Belen <>
2018-10-20 11:38:43 +0200

    mario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { interval } from "rxjs/internal/observable/interval";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'aclo-web';
+export class AppComponent implements OnInit {
+  showMario: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    interval(20000).subscribe(() => {
+      this.showMario = true;
+      setTimeout(() => {
+        this.showMario = false;
+      }, 2000);
+    });
+  }
+
 }
