6ef73dc4e6083e888bcf1a6ac01c4898b98896c5
Belen <>
2018-10-26 12:05:24 +0200

    wiii

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'acloWeb'`, async(() => {
+  it(`should have as title 'aclo-web'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('acloWeb');
+    expect(app.title).toEqual('aclo-web');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to acloWeb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aclo-web!');
   }));
 });
